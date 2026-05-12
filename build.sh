a673c799f14c65d4a3012b2e6fad29a78e61cf67
xosnfis <tasuev2007@gmail.com>
2026-05-12 22:29:31 +0500

    usl5

--- build.sh
+++ build.sh
@@ -6,9 +6,6 @@ echo "Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
-echo "Applying database migrations..."
-python manage.py migrate --no-input
-
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
